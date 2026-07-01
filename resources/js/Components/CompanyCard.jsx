3f06fbb7247e4db688b29f85356010bb7b8a28ba
mo <61505300+MoAlshehab@users.noreply.github.com>
2026-07-01 17:46:26 +0200

    with chage image source

--- resources/js/Components/CompanyCard.jsx
+++ resources/js/Components/CompanyCard.jsx
@@ -32,7 +32,7 @@ const CompanyCard = ({ company }) => {
                     {company.media.map((media, index) => (
                         <LazyLoad key={index}>
                             <img
-                                src={media.original_url.replace('localhost', '127.0.0.1:8000')}
+                                src={media.original_url}
                                 alt={`Media ${index + 1}`}
                                 className="max-w-full h-auto"
                             />
